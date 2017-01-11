e6d224ef7451c3dcde712f97ce4e6417b58aba08
Colin Arpe <colin.j.arpe@outlook.com>
2017-01-10 22:31:37 -0500

    Closes #13.  Add validations to boat model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,30 @@
 class Job < ApplicationRecord
+	include ActiveModel::Validations
+
 	has_and_belongs_to_many :boats
 	validate :minimum_cost
+	validates :description, length: {minimum: 50, too_short: "Please be more descriptive; at least %{count} characters required."}
+	validate :origin_available
+	validate :destination_available
 
 	def minimum_cost
 		if cost < 1000
 			errors.add(:cost, "Cannot charge below $1000")
+		end
+	end
+
+
+	def origin_available
+	ports = ["Shanghai", "Singapore", "Shenzhen", "Hong Kong", "Ningbo-Zhoushan", "Busan", "Qingdao", "Guangzhou", "Jebel Ali (Dubai)", "Tianjin", "Rotterdam", "Port Klang", "Kaohsiung", "Dalian", "Hamburg", "Antwerp", "Xiamen", "Los Angeles", "Tanjung Pelepas", "Long Beach"]
+		if !ports.find_index(origin)
+			errors.add(:origin, "We do not service this location")
+		end
+	end
+
+	def destination_available
+	ports = ["Shanghai", "Singapore", "Shenzhen", "Hong Kong", "Ningbo-Zhoushan", "Busan", "Qingdao", "Guangzhou", "Jebel Ali (Dubai)", "Tianjin", "Rotterdam", "Port Klang", "Kaohsiung", "Dalian", "Hamburg", "Antwerp", "Xiamen", "Los Angeles", "Tanjung Pelepas", "Long Beach"]
+		if !ports.find_index(destination)
+			errors.add(:destination, "We do not service this location")
 		end
 	end
 end
