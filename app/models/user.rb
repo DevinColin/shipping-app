190ffd9f2668ae2e8297b877602298f51c6d95b7
aisflat439 <fitzsimonsdevin@gmail.com>
2017-01-11 16:38:23 -0500

    added images for profiles with paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         has_attached_file :avatar, storage: :s3,
+          :s3_region => "us-west-2",
+          :s3_credentials => {
+            :s3_host_name       => "s3-us-west-2.amazonaws.com",
+            :bucket             => "devin-shipping-app",
+            :access_key_id      => ENV["SHIPPING_APP_ACCESS_KEY_ID"],
+            :secret_access_key  => ENV["SHIPPING_APP_AMAZON_SECRET"]
+          }
+
+  validates_attachment_content_type :avatar,
+        :content_type => /\Aimage\/.*\Z/
 end
