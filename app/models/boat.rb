ba9dab244738f2f727274a5ec4674d0014f58956
Colin Arpe <colin.j.arpe@outlook.com>
2017-01-12 13:58:49 -0500

    Need to switch branches again

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,7 +4,10 @@ class Boat < ApplicationRecord
 	PORTS = ["Shanghai", "Singapore", "Shenzhen", "Hong Kong", "Ningbo-Zhoushan", "Busan", "Qingdao", "Guangzhou", "Jebel Ali (Dubai)", "Tianjin", "Rotterdam", "Port Klang", "Kaohsiung", "Dalian", "Hamburg", "Antwerp", "Xiamen", "Los Angeles", "Tanjung Pelepas", "Long Beach"]
 
 	has_and_belongs_to_many :jobs
-	# has_attached_file :avatar
+	has_attached_file :avatar
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+	validates :name, uniqueness: true
+	validate :location_allowed
 
     has_attached_file :avatar, storage: :s3,
         :s3_region => "ca-central-1",
@@ -14,10 +17,7 @@ class Boat < ApplicationRecord
             :access_key_id      => ENV["SHIPPING_APP_ACCESS_KEY_ID"],
             :secret_access_key  => ENV["SHIPPING_APP_AMAZON_SECRET"]
         }
-		
-	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-	validates :name, uniqueness: true
-	validate :location_allowed
+	
 
 	def location_allowed
 		if !PORTS.find_index(location)
