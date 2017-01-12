3bd24871134faba467e31d0de1e30f71257d8c68
aisflat439 <fitzsimonsdevin@gmail.com>
2017-01-12 17:52:07 -0500

    Styled frontpage closes #16

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   def home
     @boat = Boat.new
     @job = Job.new
+    @jobs = Job.order(updated_at: :asc).reverse_order.limit(10).reverse
   end
 end
