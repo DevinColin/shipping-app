e1f34c148ff94a25807e1fc6cc187471cdd673f1
Colin Arpe <colin.j.arpe@outlook.com>
2017-01-12 12:15:01 -0500

    Add pull menu to boat update page, add avatar to same page

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -5,7 +5,16 @@ class Boat < ApplicationRecord
 
 	has_and_belongs_to_many :jobs
 	has_attached_file :avatar
-	
+
+    has_attached_file :avatar, storage: :s3,
+        :s3_region => "ca-central-1",
+        :s3_credentials => {
+            :s3_host_name       => "s3.#{:s3_region}.amazonaws.com",
+            :bucket             => "shipping-app-dc",
+            :access_key_id      => ENV["SHIPPING_APP_ACCESS_KEY_ID"],
+            :secret_access_key  => ENV["SHIPPING_APP_AMAZON_SECRET"]
+        }
+		
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 	validates :name, uniqueness: true
 	validate :location_allowed
